3604a5e49543d282d9136c72f7e121422d9fec18
eldhos <eldhos.kurian@borngroup.com>
2015-11-06 13:14:57 +0530

    New Extention Front end

---
+++ pms/gensrc/de/hybris/platform/pms/constants/GeneratedPmsConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 6 Nov, 2015 1:04:30 PM                      ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.platform.pms.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedPmsConstants
+{
+	public static final String EXTENSIONNAME = "pms";
+	
+	protected GeneratedPmsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ pmsfrontend/web/webroot/index.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>pmsfrontend</h1>
+		Welcome to my extension.
+	</body>
+</html>
+
